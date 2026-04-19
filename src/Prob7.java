e90374312c00fd74f22c8c5dd28aa9c7aa1590e6
Prarabdha7 <prarabdhasachancr7n1@gmail.com>
2026-04-19 23:47:52 +0530

    Week5-6Level1 probs done

---
+++ src/Prob7.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Prob7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        if (number <= 0) {
+            return;
+        }
+
+        int[] even = new int[number / 2 + 1];
+        int[] odd = new int[number / 2 + 1];
+
+        int e = 0, o = 0;
+
+        for (int i = 1; i <= number; i++) {
+            if (i % 2 == 0) {
+                even[e++] = i;
+            } else {
+                odd[o++] = i;
+            }
+        }
+
+        for (int i = 0; i < o; i++) {
+            System.out.println(odd[i]);
+        }
+
+        for (int i = 0; i < e; i++) {
+            System.out.println(even[i]);
+        }
+    }
+}
