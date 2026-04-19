7efa240542a582daafb1add8976e3be19ac261d4
Prarabdha7 <prarabdhasachancr7n1@gmail.com>
2026-04-19 23:58:34 +0530

    Week5-6Level3 probs done

---
+++ src/Prob1.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Prob1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+
+        int temp = number;
+        int count = 0;
+
+        while (temp != 0) {
+            temp /= 10;
+            count++;
+        }
+
+        int[] digits = new int[count];
+        temp = number;
+
+        for (int i = 0; i < count; i++) {
+            digits[i] = temp % 10;
+            temp /= 10;
+        }
+
+        int[] freq = new int[10];
+
+        for (int i = 0; i < count; i++) {
+            freq[digits[i]]++;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            if (freq[i] > 0) {
+                System.out.println(i + " " + freq[i]);
+            }
+        }
+    }
+}
